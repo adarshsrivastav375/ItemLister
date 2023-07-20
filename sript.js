214ed5cf3c7edc1c8ce66467c35a199c918a4155
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-07-21 03:56:01 +0530

    changed title style

---
+++ sript.js
@@ -0,0 +1,7 @@
+let headerTitle = document.getElementById('main-header');
+headerTitle.style = "border-bottom : solid 3px black";
+console.log("hello world");
+let addItem = document.getElementsByClassName('title');
+console.log(addItem);
+addItem[0].style.fontWeight="bold";
+addItem[0].style.color ="green";
