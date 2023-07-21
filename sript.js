f6eeea57ec1a11ac1c1239138c73f25428c08b14
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-07-22 01:10:11 +0530

    updated item's style in list

--- sript.js
+++ sript.js
@@ -5,3 +5,9 @@ let addItem = document.getElementsByClassName('title');
 //console.log(addItem);
 addItem[0].style.fontWeight="bold";
 addItem[0].style.color ="green";
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor ="green";
+for(let i=0; i< items.length;i++){
+    items[i].style.fontWeight = "bold";
+}
