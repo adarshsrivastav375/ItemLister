c1b09e8be52121a36800b7720dd6b1d68d9927c2
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-07-22 01:24:36 +0530

    new li added and style  manupulated by tag name

--- sript.js
+++ sript.js
@@ -5,9 +5,15 @@ let addItem = document.getElementsByClassName('title');
 //console.log(addItem);
 addItem[0].style.fontWeight="bold";
 addItem[0].style.color ="green";
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-items[2].style.backgroundColor ="green";
-for(let i=0; i< items.length;i++){
-    items[i].style.fontWeight = "bold";
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[2].style.backgroundColor ="green";
+// for(let i=0; i< items.length;i++){
+//     items[i].style.fontWeight = "bold";
+// }
+let li = document.getElementsByTagName('li');
+// console.log(items);
+li[2].style.backgroundColor ="green";
+for(let i=0; i< li.length;i++){
+    li[i].style.fontWeight = "bold";
 }
