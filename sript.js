5da80e8aa9152b9b9dd35ac5b4719e2767bad810
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-07-24 00:47:21 +0530

    querySlector method used

--- sript.js
+++ sript.js
@@ -11,9 +11,23 @@ addItem[0].style.color ="green";
 // for(let i=0; i< items.length;i++){
 //     items[i].style.fontWeight = "bold";
 // }
-let li = document.getElementsByTagName('li');
+// let li = document.getElementsByTagName('li');
 // console.log(items);
-li[2].style.backgroundColor ="green";
-for(let i=0; i< li.length;i++){
-    li[i].style.fontWeight = "bold";
-}
+// li[2].style.backgroundColor ="green";
+// for(let i=0; i< li.length;i++){
+//     li[i].style.fontWeight = "bold";
+// }
+
+//query selector
+
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor ="green";
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display ="none";
+
+let items = document.querySelectorAll('.list-group-item');
+ items[1].style = 'color : green';
+ for(let i=0; i< items.length;i=i+2){
+    items[i].style.backgroundColor = 'green';
+ }
+
